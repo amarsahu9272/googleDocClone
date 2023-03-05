1cc51a44a83a78558ffe12af4513ae65c40dcf4b
Anil Singh <anil927a@gmail.com>
2023-03-05 19:41:56 +0530

    OptimizeCode

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -5,50 +5,23 @@ import Document from "../../component/documents/Document";
 import Toolkit from "../../component/toolkit/Toolkit";
 
 function Homepage() {
-  function handleBold() {
-    document.execCommand("bold");
-  }
-  function handleUnderline() {
-    document.execCommand("underline");
-  }
 
-  function handleItalic() {
-    document.execCommand("italic");
-  }
-
-  function handlebackgroundhighlight() {
-    document.execCommand("backColor", "", "yellow");
-  }
-  function handleColor(color) {
-    document.execCommand("foreColor", "", color);
-  }
-
-  function handlejustifyCenter(color) {
-    document.execCommand("justifyCenter");
-  }
-  function handlejustifyLeft(color) {
-    document.execCommand("justifyLeft");
-  }
-  function handlejustifyRight(color) {
-    document.execCommand("justifyRight");
-  }
-  function handleremoveFormat(color) {
-    document.execCommand("removeFormat");
-  }
   return (
     <div className="homePage">
       <div className="homePageHeader">
         <TitleBar />
         <Toolkit
-          handlebold={handleBold}
-          handleUnderline={handleUnderline}
-          handleItalic={handleItalic}
-          handlebackgroundhighlight={handlebackgroundhighlight}
-          handleColor={handleColor}
-          handlejustifyCenter={handlejustifyCenter}
-          handlejustifyLeft={handlejustifyLeft}
-          handlejustifyRight={handlejustifyRight}
-          handleremoveFormat={handleremoveFormat}
+          handlebold={()=>(document.execCommand("bold"))}
+          handleItalic={() => document.execCommand("italic")}
+          handleUnderline={()=>(document.execCommand("underline"))}
+          handleColor={(color)=>(document.execCommand("foreColor", "", color))}
+          handlebackgroundhighlight={()=>(document.execCommand("backColor", "", "yellow"))}
+          
+
+          handlejustifyLeft={()=>(document.execCommand("justifyLeft"))}
+          handlejustifyCenter={()=>(document.execCommand("justifyCenter"))}
+          handlejustifyRight={()=>(document.execCommand("justifyRight"))}
+         
         />
       </div>
       <Document />
